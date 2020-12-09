dc62f7e5760e74e859caf3cdb1aeda0524527521
Everett Yeaw <evyeaw@gmail.com>
2020-12-09 17:27:56 -0500

    added and imported all components

---
+++ src/Components/Footer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+    render(){
+
+        
+        if(this.props.data){
+  
+          }
+          
+        return(
+            <div className="first row">
+               
+            </div>
+          )
+        }
+    }
+
+export default Footer;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,26 +7,22 @@ class Navbar extends Component  {
     if(this.props.data){
         var name = this.props.data.name;
         var profilepic= "images/"+this.props.data.image;
-        var bio = this.props.data.bio;
-        var street = this.props.data.address.street;
-        var city = this.props.data.address.city;
-        var state = this.props.data.address.state;
-        var zip = this.props.data.address.zip;
-        var phone= this.props.data.phone;
         var email = this.props.data.email;
-        var resumeDownload = this.props.data.resumedownload;
       }
       
     return(
         <div className="first row">
             <div className="col-med-3">
-                <span>{name}</span>
+                <span>About</span>
+            </div>
+            <div className="col-med-3">
+                <p>Skills</p>
             </div>
             <div className="col-med-3">
                 <p>Projects</p>
             </div>
             <div className="col-med-3">
-                <p>Skills</p>
+                <p>Contact</p>
             </div>
         </div>
       )
