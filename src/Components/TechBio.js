f74509ee232d86ace1f428b67744f41f56e622f5
Everett Yeaw <evyeaw@gmail.com>
2020-12-26 20:17:21 -0500

    variable name change and note to self for where to start next

--- src/Components/TechBio.js
+++ src/Components/TechBio.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react'
 class TechBio extends Component {
     render(){
       if(this.props.data){
-        var resume= this.props.data.skillmessage;
+        var resumeMessage = this.props.data.skillmessage;
+        //.map skills next
       }
 
         return(
-            <div className="first row">
-                  <p> {resume} </p>
+            <div className="content">
+                  <p> {resumeMessage} </p>
+                  <p>  </p>
             </div>
           )
         }
