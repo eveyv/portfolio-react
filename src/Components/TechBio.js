e5e7240815dcc9afc2fb0b493477e205418c8069
Everett Yeaw <evyeaw@gmail.com>
2021-01-29 10:45:21 -0500

    skills var set; can be iterated through

--- src/Components/TechBio.js
+++ src/Components/TechBio.js
@@ -4,13 +4,13 @@ class TechBio extends Component {
     render(){
       if(this.props.data){
         var resumeMessage = this.props.data.skillmessage;
+        var techSkills = this.props.data.skills;
         //.map skills here
       }
 
         return(
             <div className="content">
                 <p className="center-column"> {resumeMessage} </p>
-                //.forEach education info here
              </div>
           )
         }
